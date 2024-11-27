7d3b665a09da6603e5da8df5c71f90d8bad5069b
mmcgarry13 <mcgarry31@outlook.com>
2024-11-26 21:54:46 -0500

    made the navbar look way better

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -17,32 +17,31 @@ const Navbar = () => {
   }, [loginCheck]);
 
   return (
-    <div className='display-flex justify-space-between align-center py-2 px-5 mint-green'>
-      <h1>Authentication Review</h1>
-      <div>
-        <button className='btn' type='button'>
-          <Link to='/mainfeed'>Main Feed</Link>
+    <div className="d-flex justify-content-between align-items-center py-2 px-5" style={{ backgroundColor: '#f5f5dc' }}>
+      <h1 style={{ margin: 0 }}>Bookd</h1>
+      <div className="d-flex gap-2">
+        <button className="btn btn-primary" type="button">
+          <Link to="/mainfeed" className="text-white text-decoration-none">Main Feed</Link>
         </button>
         {!loginCheck ? (
-          <button className='btn' type='button'>
-            <Link to='/login'>Login</Link>
+          <button className="btn btn-success" type="button">
+            <Link to="/login" className="text-white text-decoration-none">Login</Link>
           </button>
         ) : (
-          <div>
-            <button className='btn' type='button'>
-              <Link to='/create'>Create</Link>
+          <>
+            <button className="btn btn-primary" type="button">
+              <Link to="/create" className="text-white text-decoration-none">Create</Link>
             </button>
             <button
-              className='btn'
-              type='button'
+              className="btn btn-danger"
+              type="button"
               onClick={() => {
                 auth.logout();
               }}
             >
               Logout
             </button>
-          </div>
-
+          </>
         )}
       </div>
     </div>
