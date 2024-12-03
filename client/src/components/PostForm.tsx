2f45b47c7ff562cf2da195743a5bce3af7170dc4
mmcgarry13 <mcgarry31@outlook.com>
2024-12-02 19:40:25 -0500

    last minute fixes to css

--- client/src/components/PostForm.tsx
+++ client/src/components/PostForm.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import {createPost, getPostByID, updatePost} from '../api/PostsAPI'; // Ensure `updatePost` is defined in your API
+import { createPost, getPostByID, updatePost } from '../api/PostsAPI'; // Ensure `updatePost` is defined in your API
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import auth from '../utils/auth';
 
-function PostForm({ mode,}: { mode: 'create' | 'update';}) {
+function PostForm({ mode, }: { mode: 'create' | 'update'; }) {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [rating, setRating] = useState(0);
@@ -42,10 +42,10 @@ function PostForm({ mode,}: { mode: 'create' | 'update';}) {
 
         try {
             if (mode == 'create') { await createPost({ postUser, title, author, rating, review }); }
-            
-            else if (id && mode == 'update') { 
+
+            else if (id && mode == 'update') {
                 console.log('new data: ', author);
-                await updatePost(id, { title, author, rating, review, postUser }); 
+                await updatePost(id, { title, author, rating, review, postUser });
             }
 
             navigate('/'); // go back home.
@@ -56,66 +56,68 @@ function PostForm({ mode,}: { mode: 'create' | 'update';}) {
     };
 
     return (
-        <div className="container">
-            <h1 className="text-center">{mode === 'create' ? 'Create a New Post' : 'Update Post'}</h1>
-            <form onSubmit={handleFormSubmit} className="border">
-                <div className="mb-3">
-                    <label className="form-label">Title</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="title"
-                        placeholder="Enter title"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                </div>
+        <div className="container-fluid min-vh-100">
+            <div className="container">
+                <h1 className="text-center">{mode === 'create' ? 'Create a New Post' : 'Update Post'}</h1>
+                <form onSubmit={handleFormSubmit} className="border">
+                    <div className="mb-3">
+                        <label className="form-label">Title</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="title"
+                            placeholder="Enter title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
+                    </div>
 
-                <div className="mb-3">
-                    <label className="form-label">Author</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="author"
-                        placeholder="Enter author"
-                        value={author}
-                        onChange={(e) => setAuthor(e.target.value)}
-                    />
-                </div>
+                    <div className="mb-3">
+                        <label className="form-label">Author</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="author"
+                            placeholder="Enter author"
+                            value={author}
+                            onChange={(e) => setAuthor(e.target.value)}
+                        />
+                    </div>
 
-                <div className="mb-3">
-                    <label className="form-label">Rating (out of 10)</label>
-                    <select
-                        className="form-select"
-                        id="rating"
-                        value={rating || ''}
-                        onChange={(e) => setRating(parseInt(e.target.value))}
-                    >
-                        <option value="">Select a rating</option>
-                        {[...Array(10)].map((_, i) => (
-                            <option key={i + 1} value={i + 1}>
-                                {i + 1}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+                    <div className="mb-3">
+                        <label className="form-label">Rating (out of 10)</label>
+                        <select
+                            className="form-select"
+                            id="rating"
+                            value={rating || ''}
+                            onChange={(e) => setRating(parseInt(e.target.value))}
+                        >
+                            <option value="">Select a rating</option>
+                            {[...Array(10)].map((_, i) => (
+                                <option key={i + 1} value={i + 1}>
+                                    {i + 1}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
 
-                <div className="mb-3">
-                    <label className="form-label">Review</label>
-                    <textarea
-                        className="form-control"
-                        id="review"
-                        placeholder="Write your review here"
-                        value={review}
-                        onChange={(e) => setReview(e.target.value)}
-                    ></textarea>
-                </div>
+                    <div className="mb-3">
+                        <label className="form-label">Review</label>
+                        <textarea
+                            className="form-control"
+                            id="review"
+                            placeholder="Write your review here"
+                            value={review}
+                            onChange={(e) => setReview(e.target.value)}
+                        ></textarea>
+                    </div>
 
 
-                <button type="submit" className="btn btn-primary">
-                    {mode === 'create' ? 'Submit' : 'Update'}
-                </button>
-            </form>
+                    <button type="submit" className="btn btn-primary">
+                        {mode === 'create' ? 'Submit' : 'Update'}
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
