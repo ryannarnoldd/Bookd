09951b0211eb4fb69ecd1e28330215d5480177f5
mmcgarry13 <mcgarry31@outlook.com>
2024-12-02 20:40:44 -0500

    got that pesky quotation mark

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -30,7 +30,7 @@ const Post = ({ id, postUser, title, author, rating, review, onDelete }: PostPro
 
     return (
         // <div className="container-fluid min-vh-100">
-        <div className="col-md-6 mb-4">"
+        <div className="col-md-6 mb-4">
             <div className="card shadow-sm">
                 <div className="card-body" style={{ backgroundColor: '#F5F5DC' }}>
                     <h5 className="card-title text-primary">
